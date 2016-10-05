21a7e12d97eee85bcb078901cbfaf86006d511f3
BHess2653 <GM_Dahaka@BHMac.local>
2016-10-05 00:42:08 -0400

    updated server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const body_parser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // Port set to 3000
 const port = process.env.PORT || 3000;
 
-app.use(body_parser.urlencoded({
-  extended: true
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
 }));
-app.use(body_parser.json());
 
-app.use('/api/v1', require('./routes/api/urls')(express));
+app.use('/', require('./routes')(express));
 
 const server = app.listen(port, () => {
   console.log('Server Active on port ', port);
